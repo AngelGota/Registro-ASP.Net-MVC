27f724d594cc8cc3b2f4862373ad8bbb45ceeb7f
Angelo <gangelos630@gmail.com>
2023-05-06 11:51:11 -0500

    Implementation of password to db and model

--- LoginWebAppMVC.sql
+++ LoginWebAppMVC.sql
@@ -1,30 +1,38 @@
-
+-- Deletion of BD
+Drop database LoginWebApp
+GO
+-- Creation of BD
 Create database LoginWebApp
+Go
+
 Use LoginWebApp
+Go
 
 Create table Users(
 idUser int identity (1,1),
 namUser varchar(50),
 ageUser int,
-mailUser varchar(50)
+mailUser varchar(50),
+passUser varchar(30)
 )
 
-Create procedure sp_reg_user
+Create procedure sp_register_users
 @namUser varchar(50),
 @ageUser int,
-@mailUser varchar(50) 
+@mailUser varchar(50),
+@passUser varchar(30)
 as begin
-insert into Users values(@namUser, @ageUser,@mailUser)
+insert into Users values(@namUser, @ageUser,@mailUser,@passUser)
 end
 
-exec sp_reg_user 'Gota',19,'gota@dominio.com'
+exec sp_register_users 'Gota',19,'gota@dominio.com','mypassword'
 go
 
 select * from Users
 
-Create procedure sp_rea_user
+Create procedure sp_read_users
 as begin
 select * from Users
 end
 
-exec sp_rea_user
+exec sp_read_users
